cd1742a34a9ea6826649a4ee2aa7eab7bf6d30e1
Oualid-A <oualid.achrif9@gmail.com>
2024-03-17 18:46:36 +0100

    add dashboard and Trainintg types

--- src/app/dashboard/components/tracking/tracking.component.ts
+++ src/app/dashboard/components/tracking/tracking.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ELEMENT_DATA, FoodItem } from './tracking.modell';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTooltip,
+  ApexStroke
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+};
 
 @Component({
   selector: 'mg-tracking',
   templateUrl: './tracking.component.html',
 })
 export class TrackingComponent {
+onToggleChangeDate(arg0: string) {
+throw new Error('Method not implemented.');
+}
   height = new FormControl('', Validators.required);
   weight = new FormControl('', Validators.required);
+  classification: string;
+  bmi: number = 0;
+  stepsCount: number = 9870;
+  distanceCovered: number = 8.5; // en kilomètres
+  caloriesBurned: number = 530;
+  workoutsCompleted: number = 5;
+  displayedColumns: string[] = [
+    'name',
+    'protein',
+    'calories',
+    'fat',
+  ];
+  dataSource = new MatTableDataSource<FoodItem>(ELEMENT_DATA);
 
-  bmi: number | null = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   calculate() {
     const heightMeters = this.height.value;
@@ -25,5 +62,18 @@ export class TrackingComponent {
           ((heightMetersNumber / 100) * (heightMetersNumber / 100));
       }
     }
+    if (this.bmi < 16) {
+      this.classification = 'Severe thinness';
+    } else if (this.bmi > 16 && this.bmi < 30) {
+      this.classification = 'Normal weight';
+    } else {
+      this.classification = 'Overweight ';
+    }
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  
 }
--- src/app/dashboard/components/training-types/training-types.component.ts
+++ src/app/dashboard/components/training-types/training-types.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatChip } from '@angular/material/chips';
+import { FormControl } from '@angular/forms';
+import { TrainingService } from '../../training.service';
+import { Exercise } from './exercise.model';
 
 @Component({
   selector: 'mg-training-types',
   templateUrl: './training-types.component.html',
 })
 export class TrainingTypesComponent {
+  responseData: Exercise[];
 
+  chipListControl = new FormControl();
+
+  selectedValue: string = 'Back';
+  private readonly http = inject(HttpClient);
+  private readonly trainingService = inject(TrainingService);
+
+  ngOnInit() {
+    this.chipListControl.valueChanges.subscribe((value) => {
+      this.selectedValue = value;
+    });
+
+    this.trainingService.getTrainingData(this.selectedValue).subscribe(
+      (response: any) => {
+        this.responseData = response;
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  sendRequest(): void {
+    this.trainingService.getTrainingData(this.selectedValue).subscribe(
+      (response: Exercise[]) => {
+        this.responseData = response;
+        console.log(this.responseData);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
+
+
