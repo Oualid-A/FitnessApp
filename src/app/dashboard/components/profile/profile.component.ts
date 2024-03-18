67960366b9c420404b8373eb4b611b21c95fed33
Oualid-A <oualid.achrif@ump.ac.ma>
2024-03-18 11:57:15 +0100

    add profile page

--- src/app/dashboard/components/profile/profile.component.ts
+++ src/app/dashboard/components/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'mg-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
+  name = "Achrif";
+  prenom = "Oualid";
+  email = "oualid@gmail.com";
 
+  
 }
